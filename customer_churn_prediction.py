28f27347ff13a72316c924bb2d86b32201ce6aad
Rupali <guptarupali@gmail.com>
2021-12-22 00:09:58 +0530

    Code checkin for Project

---
+++ customer_churn_prediction.py
@@ -0,0 +1,377 @@
+# **********Step 1: Problem Definition****************
+# The key challenge is to predict if an individual customer will churn or not.
+# To accomplish that, machine learning models are trained based on 80% of the sample data.
+# The remaining 20% are used to apply the trained models and assess their predictive power with regards to "churn / not churn".
+# We will also determine features which actually drive customer churn.
+
+# **********Step 2: Data Collection*******************
+import inline as inline
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+
+# Model training
+from sklearn.model_selection import train_test_split
+
+# Model Evaluation
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, roc_auc_score
+from sklearn.metrics import precision_recall_curve, auc, f1_score, plot_confusion_matrix, precision_score, recall_score
+
+# Model Selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+# Forward Neural Network
+from keras.models import Sequential, load_model
+from keras import layers
+from keras.layers.core import Dropout
+from keras.callbacks import ModelCheckpoint
+from tensorflow.python import tf2
+
+df = pd.read_csv('customer-churn-data.csv')
+
+#***********Step 3: Exploratory Data Analysis***********
+# Show the features (=columns) and first data entries of the data frame.
+df.head(5)
+
+# Get a summary on the data frame incl. data types, shape and memory storage.
+df.info()
+
+# Get statistical information on numerical features.
+df.describe()
+
+# Looping through the columns to get unique values per column.
+for i in df.columns:
+    print(f"Unique {i}'s count: {df[i].nunique()}")
+    print(f"{df[i].unique()}\n")
+
+#*****************Data Preprocessing for EDA*********************
+
+# Changing the data tyoe of "TotalCharges" and "tenure" to float.
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+
+def feature_to_float(feature_list, df):
+    for i in feature_list:
+        df[i] = df[i].astype(float)
+    return df
+
+feature_to_float(['tenure'], df)
+
+# Renaming the data values of "PaymentMethod" for better readability.
+payment_column = {'Electronic check': 'E-Check' , 'Mailed check': 'Mailed Check', 'Bank transfer (automatic)': 'Bank Transfer', 'Credit card (automatic)': 'Credit Card'}
+df["PaymentMethod"].replace(payment_column, inplace=True)
+
+# Checking the data types for any unintended data types.
+df.dtypes
+
+# Counting the number of missing values.
+df.isna().sum()
+
+#*****************Data Exploration*********************
+
+# Apply the Fivethirtyeight style to all plots.
+plt.style.use("fivethirtyeight")
+
+# Display a frequency distribution for churn.
+plt.figure(figsize=(5, 5))
+ax = sns.countplot(x=df['Churn'], palette="Blues", linewidth=1)
+plt.show()
+
+# Create a function to generate boxplots.
+plots = {1: [111], 2: [121, 122], 3: [131, 132, 133], 4: [221, 222, 223, 224], 5: [231, 232, 233, 234, 235],
+         6: [231, 232, 233, 234, 235, 236]}
+
+
+def boxplot(x, y, df):
+    rows = int(str(plots[len(y)][0])[0])
+    columns = int(str(plots[len(y)][0])[1])
+    plt.figure(figsize=(7 * columns, 7 * rows))
+
+    for i, j in enumerate(y):
+        plt.subplot(plots[len(y)][i])
+        ax = sns.boxplot(x=x, y=j, data=df[[x, j]], palette="Blues", linewidth=1)
+        ax.set_title(j)
+
+    return plt.show()
+
+# Generate boxplots for tenure, MonthlyCharges and TotalCharges.
+boxplot("Churn", ["tenure", "MonthlyCharges", "TotalCharges"], df)
+
+
+# Create a function to generate countplots:
+def countplot(x, y, df):
+    rows = int(str(plots[len(y)][0])[0])
+    columns = int(str(plots[len(y)][0])[1])
+
+    plt.figure(figsize=(7 * columns, 7 * rows))
+
+    for i, j in enumerate(y):
+        plt.subplot(plots[len(y)][i])
+        ax = sns.countplot(x=j, hue=x, data=df, palette='Blues', alpha=0.8, linewidth=0.4, edgecolor='black')
+        ax.set_title(j)
+
+    return plt.show()
+
+# Generate countplots for various features.
+countplot("Churn", ['SeniorCitizen', 'Contract', 'Partner', 'Dependents', 'PaymentMethod', 'InternetService'], df)
+
+#****************Check for Outliers in Numerical Features****************************
+
+# Check of outliers by applying the IQR method checking if values are way outside the IQR borders.
+numerical_features = ["tenure", "MonthlyCharges", "TotalCharges"]
+df_num = df[numerical_features]
+df_num.describe()
+
+#*************************Data Cleaning**************************
+
+# Drop the rows with missing values.
+df = df.dropna()
+
+#**********************Step 4: Feature Engineering****************
+
+# Drop customerID feature.
+df = df.drop(columns='customerID')
+# Generate new feature "Number_AdditionalServices" by summing up the number of add-on services consumed.
+df['Number_AdditionalServices'] = (df[['OnlineSecurity', 'DeviceProtection', 'StreamingMovies', 'TechSupport', 'StreamingTV', 'OnlineBackup']] == 'Yes').sum(axis=1)
+
+# Generate countplot for the new feature.
+countplot('Churn', ['Number_AdditionalServices'], df)
+
+# Label encoding for identified columns.
+features_le = ['gender', 'Partner', 'Dependents', 'Churn', 'PhoneService', 'PaperlessBilling']
+
+def label_encoding(features, df):
+    for i in features:
+        df[i] = df[i].map({'Yes': 1, 'No': 0})
+    return
+
+label_encoding(['Partner', 'Dependents', 'Churn', 'PhoneService', 'PaperlessBilling'], df)
+
+df['gender'] = df['gender'].map({'Female': 1, 'Male': 0})
+
+# One-Hot-Encoding for identified columns.
+features_ohe = ['MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaymentMethod', 'Number_AdditionalServices']
+
+df = pd.get_dummies(df, columns=features_ohe)
+
+# Min-Max-Scaling for identified columns.
+
+
+features_mms = ['tenure', 'MonthlyCharges', 'TotalCharges']
+df_features_mms = pd.DataFrame(df, columns=features_mms)
+df_remaining_features = df.drop(columns=features_mms)
+
+mms = MinMaxScaler()
+rescaled_features = mms.fit_transform(df_features_mms)
+
+df_rescaled_features = pd.DataFrame(rescaled_features, columns=features_mms, index=df_remaining_features.index)
+
+df = pd.concat([df_remaining_features, df_rescaled_features], axis=1)
+
+#************************Step 5: Train-Test-Split***************************
+
+#Applying sklearn's splitter function train_test_split
+
+X1 = df.drop('Churn', axis=1)
+X = X1.values
+y = df['Churn'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+#****************Step 6: Model Evaluation Metrics*******************
+
+# Define a function that plots the feature weights for a classifier.
+def feature_weights(X_df, classifier, classifier_name):
+    weights = pd.Series(classifier.coef_[0], index=X_df.columns.values).sort_values(ascending=False)
+
+    top_weights_selected = weights[:10]
+    plt.figure(figsize=(7, 6))
+    plt.tick_params(labelsize=10)  # plt.xlabel(fontsize=10)
+    plt.title(f'{classifier_name} - Top 10 Features')
+    top_weights_selected.plot(kind="bar")
+
+    bottom_weights_selected = weights[-10:]
+    plt.figure(figsize=(7, 6))
+    plt.tick_params(labelsize=10)  # plt.xlabel(fontsize=10)
+    plt.title(f'{classifier_name} - Bottom 10 Features')
+    bottom_weights_selected.plot(kind="bar")
+
+    return print("")
+
+
+# Define a function that plots the confusion matrix for a classifier and the train and test accuracy
+def confusion_matrix_plot(X_train, y_train, X_test, y_test, classifier, y_pred, classifier_name):
+    fig, ax = plt.subplots(figsize=(7, 6))
+    plot_confusion_matrix(classifier, X_test, y_test, display_labels=["No Churn", "Churn"], cmap=plt.cm.Blues,
+                          normalize=None, ax=ax)
+    ax.set_title(f'{classifier_name} - Confusion Matrix')
+    plt.show()
+
+    fig, ax = plt.subplots(figsize=(7, 6))
+    plot_confusion_matrix(classifier, X_test, y_test, display_labels=["No Churn", "Churn"], cmap=plt.cm.Blues,
+                          normalize='true', ax=ax)
+    ax.set_title(f'{classifier_name} - Confusion Matrix (norm.)')
+    plt.show()
+
+    print(f'Accuracy Score Test: {accuracy_score(y_test, y_pred)}')
+    print(f'Accuracy Score Train: {classifier.score(X_train, y_train)} (as comparison)')
+    return print("")
+
+
+# Define a function that plots the ROC curve and the AUC score
+def roc_curve_auc_score(X_test, y_test, y_pred_probabilities, classifier_name):
+    y_pred_prob = y_pred_probabilities[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr, tpr, label=f'{classifier_name}')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'{classifier_name} - ROC Curve')
+    plt.show()
+
+    return print(f'AUC Score (ROC): {roc_auc_score(y_test, y_pred_prob)}\n')
+
+
+# Define a function that plots the precision-recall-curve and the F1 score and AUC score
+def precision_recall_curve_and_scores(X_test, y_test, y_pred, y_pred_probabilities, classifier_name):
+    y_pred_prob = y_pred_probabilities[:, 1]
+    precision, recall, thresholds = precision_recall_curve(y_test, y_pred_prob)
+
+    plt.plot(recall, precision, label=f'{classifier_name}')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title(f'{classifier_name} - Precision-Recall Curve')
+    plt.show()
+
+    f1_score_result, auc_score_result = f1_score(y_test, y_pred), auc(recall, precision)
+
+    return print(f'F1 Score: {f1_score_result} \nAUC Score (PR): {auc_score_result}\n')
+
+# ******************Step 7: Model Selection, Training, Prediction and Assessment************************
+
+# K Nearest Neighbors
+# Instanciate and train the KNN classifier based on the training set.
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+# Make predictions (classes and probabilities) with the trained classifier on the test set.
+y_pred_knn = knn.predict(X_test)
+y_pred_knn_prob = knn.predict_proba(X_test)
+# Plot model evaluations.
+confusion_matrix_plot(X_train, y_train, X_test, y_test, knn, y_pred_knn, 'KNN')
+roc_curve_auc_score(X_test, y_test, y_pred_knn_prob, 'KNN')
+precision_recall_curve_and_scores(X_test, y_test, y_pred_knn, y_pred_knn_prob, 'KNN')
+
+#***************Logistic Regression*********************
+
+# Instanciate and train the logistic regression model based on the training set.
+logreg = LogisticRegression(max_iter=1000)
+logreg.fit(X_train, y_train)
+LogisticRegression(max_iter=1000)
+
+# Make predictions (classes and probabilities) with the trained model on the test set.
+y_pred_logreg = logreg.predict(X_test)
+y_pred_logreg_prob = logreg.predict_proba(X_test)
+
+# Plot model evaluations.
+feature_weights(X1, logreg, 'Log. Regression')
+confusion_matrix_plot(X_train, y_train, X_test, y_test, logreg, y_pred_logreg, 'Log. Regression')
+roc_curve_auc_score(X_test, y_test, y_pred_logreg_prob, 'Log. Regression')
+precision_recall_curve_and_scores(X_test, y_test, y_pred_logreg, y_pred_logreg_prob, 'Log. Regression')
+
+# Random Forest
+# Instanciate and train the random forest model based on the training set.
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+# Make predictions (classes and probabilities) with the trained model on the test set.
+y_pred_rf = rf.predict(X_test)
+y_pred_rf_prob = rf.predict_proba(X_test)
+# Plot model evaluations.
+confusion_matrix_plot(X_train, y_train, X_test, y_test, rf, y_pred_rf, 'Random Forest')
+roc_curve_auc_score(X_test, y_test, y_pred_rf_prob, 'Random Forest')
+precision_recall_curve_and_scores(X_test, y_test, y_pred_rf, y_pred_rf_prob, 'Random Forest')
+
+# Support Vector Machine
+
+# Instanciate and train the SVM model on the training set.
+support_vector_m = SVC(kernel='rbf', probability=True)
+support_vector_m.fit(X_train,y_train)
+# Make predictions (classes and probabilities) with the trained model on the test set.
+y_pred_svm = support_vector_m.predict(X_test)
+y_pred_svm_prob = support_vector_m.predict_proba(X_test)
+# Plot model evaluations.
+confusion_matrix_plot(X_train, y_train, X_test, y_test, support_vector_m, y_pred_svm, 'SVM')
+roc_curve_auc_score(X_test, y_test, y_pred_svm_prob, 'SVM')
+precision_recall_curve_and_scores(X_test, y_test, y_pred_svm, y_pred_svm_prob, 'SVM')
+
+# Feed Forward Neural Network
+# Instanciate NN, build up layer structure and compile model
+nn = Sequential()
+Input_Shape = X_train.shape[1]
+
+nn.add(layers.Dense(1024, input_shape=(Input_Shape,), activation='relu'))
+nn.add(Dropout(0.2))
+nn.add(layers.Dense(1024, activation='relu'))
+nn.add(Dropout(0.2))
+nn.add(layers.Dense(1, activation='sigmoid'))
+nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+nn.summary()
+
+# Set callback to store model with best validation accuracy during training.
+mc = ModelCheckpoint('best_model.h5', monitor='val_accuracy', mode='max', verbose=0, save_best_only=True)
+
+# Train model and store best performing version
+fit_nn = nn.fit(X_train, y_train,
+          epochs=100,
+          verbose=False,
+          validation_data=(X_test, y_test),
+          batch_size=30, callbacks=[mc])
+
+best_nn = load_model('best_model.h5')
+
+# Plot model evaluation of best and final model after completed training.
+accuracy = best_nn.evaluate(X_train, y_train, verbose=False)
+print("Best Model Training Score: {:.4f}".format(accuracy[0]))
+print("Best Model Training Accuracy: {:.4f}".format(accuracy[1]))
+
+accuracy = best_nn.evaluate(X_test, y_test, verbose=False)
+print("Best Model Testing Score: {:.4f}".format(accuracy[0]))
+print("Best Model Testing Accuracy: {:.4f}".format(accuracy[1]))
+print("-----------------------------------------")
+
+accuracy = nn.evaluate(X_train, y_train, verbose=False)
+print("Final Model Training Score: {:.4f}".format(accuracy[0]))
+print("Final Model Training Accuracy: {:.4f}".format(accuracy[1]))
+
+accuracy = nn.evaluate(X_test, y_test, verbose=False)
+print("Final Model Testing Score: {:.4f}".format(accuracy[0]))
+print("Final Model Testing Accuracy: {:.4f}".format(accuracy[1]))
+
+# Define funtion to show history of loss and accuracy during entire model training.
+def plot_history(fit_keras):
+    acc = fit_keras.history['accuracy']
+    val_acc = fit_keras.history['val_accuracy']
+    loss = fit_keras.history['loss']
+    val_loss = fit_keras.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(14, 7))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training Acc.')
+    plt.plot(x, val_acc, 'r', label='Testing Acc.')
+    plt.title('Training & Testing Accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training Loss')
+    plt.plot(x, val_loss, 'r', label='Testing Loss')
+    plt.title('Training & Testing Accuracy')
+    plt.legend()
+
+    plot_history(fit_nn)
